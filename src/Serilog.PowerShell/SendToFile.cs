84304d681762700a6d03bd2d3d65bfd020292ec3
Matthew Erbs <matthew.erbs@merbla.com>
2015-04-23 20:21:29 +1000

    First cut with cmdlets

---
+++ src/Serilog.PowerShell/SendToFile.cs
@@ -0,0 +1,50 @@
+﻿using System.Management.Automation;
+
+namespace Serilog.PowerShell
+{
+    [Cmdlet(VerbsCommunications.Send, "ToFile")]
+    public class SendToFile : PSCmdlet
+    {
+        [Parameter(Position = 1, Mandatory = true,
+            ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The path of the file to write to")]
+        [ValidateNotNullOrEmpty]
+        public string FilePath
+        {
+            get;
+            set;
+        }
+
+        [Parameter(ValueFromPipeline = true, 
+            ParameterSetName = "FromPipeline", 
+            Mandatory = true)]
+        [ValidateNotNullOrEmpty]
+        public object InputObject { get; set; }
+
+        protected override void BeginProcessing()
+        {
+            base.BeginProcessing();
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.File(FilePath)
+                .CreateLogger();
+        }
+
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+
+            Log.Information("{0}", InputObject);
+        }
+
+        protected override void EndProcessing()
+        {
+            base.EndProcessing();
+
+          //TODO: Dispose??
+            Log.Logger = null;
+        }
+
+
+    }
+}
---
+++ src/Serilog.PowerShell/SendToSplunk.cs
@@ -0,0 +1,59 @@
+﻿#region
+
+using System.Management.Automation;
+
+#endregion
+
+namespace Serilog.PowerShell
+{
+    [Cmdlet(VerbsCommunications.Send, "ToSplunk")]
+    public class SendToSplunk : PSCmdlet
+    {
+        [Parameter(Position = 1, Mandatory = true,
+              ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The host name of the Splunk Instance")]
+        [ValidateNotNullOrEmpty]
+        public string SplunkHost { get; set; }
+
+        [Parameter(Position = 2, Mandatory = true,
+              ValueFromPipelineByPropertyName = true,
+            HelpMessage = "The post of the Splunk Instance")]
+        [ValidateNotNullOrEmpty]
+        public int Port { get; set; }
+
+        [Parameter(ValueFromPipeline = true, 
+            ParameterSetName = "FromPipeline", 
+            Mandatory = true)]
+        [ValidateNotNullOrEmpty]
+        public object InputObject { get; set; }
+
+        protected override void ProcessRecord()
+        {
+            base.ProcessRecord();
+            Log.Information("{0}", InputObject);
+        }
+
+        protected override void BeginProcessing()
+        {
+            base.BeginProcessing();
+
+            WriteVerbose("Setting up Serilog with Splunk");
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.SplunkViaTcp(SplunkHost, Port)
+                .CreateLogger();
+        }
+
+        protected override void EndProcessing()
+        {
+            base.EndProcessing();
+           // Log.Logger = null;
+        }
+
+        protected override void StopProcessing()
+        {
+            base.StopProcessing();
+           // Log.Logger = null;
+        }
+    }
+}
